0439d51c6c4c459bf7da0f564025ee10ff66c0e0
George Nosachev <nosachev.gosha@gmail.com>
2022-10-26 14:22:19 +0300

    feat: formed lib structure

---
+++ lib/internal/dependencies/api_module.dart
@@ -0,0 +1,13 @@
+// import '../../data/api/api_util.dart';
+// import '../../data/api/service/sunrise_service.dart';
+
+// class ApiModule {
+//   static ApiUtil _apiUtil;
+
+//   static ApiUtil apiUtil() {
+//     if (_apiUtil == null) {
+//       _apiUtil = ApiUtil(SunriseService());
+//     }
+//     return _apiUtil;
+//   }
+// }
---
+++ lib/internal/dependencies/repository_module.dart
@@ -0,0 +1,17 @@
+// import '../../data/repository/day_data_repository.dart';
+// import '../../domain/repository/day_repository.dart';
+
+// import 'api_module.dart';
+
+// class RepositoryModule {
+//   static DayRepository _dayRepository;
+
+//   static DayRepository dayRepository() {
+//     if (_dayRepository == null) {
+//       _dayRepository = DayDataRepository(
+//         ApiModule.apiUtil(),
+//       );
+//     }
+//     return _dayRepository;
+//   }
+// }
