601f1cf995315ea11a6f2dd4833e5c57947fcd88
George Nosachev <nosachev.gosha@gmail.com>
2022-10-26 23:55:01 +0300

    feat: added sample layouts

--- README.md
+++ README.md
@@ -1,16 +1,10 @@
-# course_example_app
+# TuneLike
 
-A new Flutter project.
+Mobile client application of the TuneLike social platform
 
 ## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
 
 - [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
 - [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+[online documentation](https://docs.flutter.dev/)
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,52 @@
+import 'package:flutter/material.dart';
+
 import 'firebase_options.dart';
 import 'package:soar_quest/soar_quest.dart';
 
+late SQCollection categories, courses, lessons;
+
 void main() async {
-  await SQApp.init("Course App",
+  await SQApp.init("TuneLike",
+      userDocFields: [SQStringField("Telegram Username")],
       firebaseOptions: DefaultFirebaseOptions.currentPlatform);
-  SQApp.run(SQNavBar([Screen("Test Screen"), Screen("Test Screen 2")]));
+
+  await UserSettings.setSettings([
+    SQBoolField("Visual Impairment", value: false),
+  ]);
+
+  categories = FirestoreCollection(id: "Categories", fields: [
+    SQStringField("Title"),
+    SQStringField("Description"),
+    SQImageField("Image"),
+  ]);
+
+  courses = FirestoreCollection(id: "Courses", fields: [
+    SQStringField("Title"),
+    SQStringField("Description"),
+    SQRefField("Categories", collection: categories),
+    SQRefDocsField("Lessons",
+        refCollection: () => lessons, refFieldName: "Course"),
+  ], actions: [
+    GoScreenAction("Go to Lessons",
+        icon: Icons.note,
+        screen: (courseDoc) => CollectionScreen(collection: lessons))
+  ]);
+
+  lessons = FirestoreCollection(id: "Lessons", fields: [
+    SQStringField("Title"),
+    SQStringField("Description"),
+    SQRefField("Course", collection: courses),
+    VideoLinkField("Video Link"),
+  ]);
+
+  SQApp.run(
+      SQNavBar(
+        [
+          GalleryScreen(collection: categories),
+          GalleryScreen(collection: courses),
+          // GalleryScreen(collection: lessons),
+          UserSettings.settingsScreen(),
+        ],
+      ),
+      drawer: SQDrawer([]));
 }
